6868eea03ac8d39f160c68838fa3783e4e46a619
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-01-07 16:42:36 -0300

    adicionada as rotas

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <span>About</span>
+      </div>
+    );
+  }
+}
+
+export default About;
